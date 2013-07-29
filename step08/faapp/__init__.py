c143f97d699462848694af9553a535478af82cc3
Petr Blahos <petrblahos@gmail.com>
2013-07-29 21:16:39 +0200

    This step uses custom context factories for routes.
    The data lookup code has moved from the views to
    the context factory.

---
+++ step08/faapp/__init__.py
@@ -0,0 +1,19 @@
+from pyramid.config import Configurator
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('top', '/')
+
+    config.add_route("list", "/list/{model}", factory="faapp.model.resources.Context", )
+    config.add_route("edit", "/edit/{model}", factory="faapp.model.resources.Context", )
+    config.add_route("delete", "/delete/{model}", factory="faapp.model.resources.Context", )
+
+    config.include("faapp.locale")
+    config.include("faapp.model")
+
+    config.scan()
+    return config.make_wsgi_app()
+
